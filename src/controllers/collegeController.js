44472f5776e69bf232500c538d9bfbf93968968a
RaviiSharma <engineerravi036@gmail.com>
2022-09-15 17:09:11 +0530

    all complete

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -1,6 +1,6 @@
 const collegeModel = require("../models/collegeModel");
-const internModel = require("../models/internModel")
-const url = require("valid-url")
+const internModel = require("../models/internModel");
+const url = require("valid-url");
 
 const createCollege = async (req, res) => {
   try {
@@ -8,121 +8,103 @@ const createCollege = async (req, res) => {
 
     const { name, fullName, logoLink } = data; //destructuring required fields
 
-    // in case name is not given
-    if (!name) {
+    // in case name is not given or not correct
+    if (!name || !name.match(/^[a-z]((?![? .,'-]$)[ .]?[a-z]){1,10}$/g)) {
       return res
         .status(400)
-        .send({ status: false, msg: "name is a required field" });
-    }
-
-    //creating pattern for valid name
-    const namePattern = /^[a-z]((?![? .,'-]$)[ .]?[a-z]){1,10}$/g;
-    // checking if the name is valid
-    if (!name.match(namePattern)) {
-      return res
-        .status(400)
-        .send({ status: false, msg: "This is not a valid Name" });
+        .send({ status: false, msg: "Name is mandatory & should be unique" });
     }
     //in case full name is not present
-    if (!fullName) {
+    if (
+      !fullName ||
+      !fullName.match(/^[a-z]((?![? .,'-]$)[ .]?[a-z]){3,150}$/gi)
+    ) {
       return res
         .status(400)
-        .send({ status: false, msg: "fullName is a required field" });
-    }
-    //creating pattern for valid fullname -case insentitive
-    const fullNamePattern = /^[a-z]((?![? .,'-]$)[ .]?[a-z]){3,150}$/gi;
-    //valdiating the full name
-    if (!fullName.match(fullNamePattern)) {
-      return res
-        .status(400)
-        .send({ status: false, msg: "This is not a valid full Name" });
+        .send({ status: false, msg: "Full name is mandatory " });
     }
     //if the logoLink is not present
-    if (!logoLink) {
+    if (!logoLink || !url.isUri(logoLink)) {
       return res
         .status(400)
-        .send({ status: false, msg: "logoLink is a required field" });
+        .send({ status: false, msg: "Logo link is mandatory" });
     }
-
-    //using valid-url to valdiate the email
-    const validateLogoLink = url.isUri(logoLink);
-
-    if (!validateLogoLink) {
-      return res
-        .status(400)
-        .send({ status: false, msg: "This is not a valid logoLink" });
-    }
-    //in case this college already does not exists
-    if (!await collegeModel.exists({ name: data.name })) {
+    //in case this college does not exists
+    if (!(await collegeModel.exists({ name: data.name }))) {
       // creating a new college document
       let college = await collegeModel.create(data);
       //sending the response
-      return res
-        .status(201)
-        .send({
-          status: true,
-          msg: "Your college has been registered",
-          data: college,
-        });
+      return res.status(201).send({
+        status: true,
+        msg: "Your college has been registered",
+        data: college,
+      });
     } else {
       //response in case the data already exists in db
-      return res
-        .status(400)
-        .send({
-          status: false,
-          msg: "this college name is already registered",
-        });
+      return res.status(400).send({
+        status: false,
+        msg: "this college name is already registered",
+      });
     }
   } catch (err) {
     return res.status(500).send({ status: false, err: err.message });
   }
 };
 
+const collegeDetails = async (req, res) => {
+  try {
+    let data = req.query;
+    let { collegeName } = data; //destructuring the collegeName from the data
 
-const collegeDetails = async (req,res)=>{
-    try {
-        let data = req.query
-        let {collegeName} = data //destructuring the collegeName from the data
-         
-        if(!collegeName){
-             return res.status(400).send({status : false, msg : "College Name is required to perform this action"})
-         } //response in case college name is not present
-
-        let namePattern = /^[a-z]((?![? .,'-]$)[ .]?[a-z]){1,10}$/g //creating a valid name pattern
-        
-        if(!collegeName.match(namePattern)){
-             return res.status(400).send({status : false, msg : "This is not a valid college Name"})
-        } // matching the name we got in the request again the valid pattern and the response if it fails. 
-        // in case it matches then we will try to find the college with that name we were given in the request
-        
-        let findcollege = await collegeModel.findOne({name: collegeName, isDeleted: false}).select({_id: 1, name: 1, logoLink: 1, fullName: 1}) //selecting the required fields
-
-        if(!findcollege){
-            return res.status(400).send({ status: false, msg: "No college with this name exists" })
-        } // in caase no college was found, this would be our response
-
-        let collegeId = findcollege._id //storing the id of the college with find from the name inside a variable collegeId
-
-        let candidates = await internModel.find({collegeId : collegeId, isDeleted : false}).select({name : 1, email : 1, mobile : 1})
-        //searching for candidates inside the intern model and selecting their name, email, and mobiles
-        if(!candidates.length){
-            return res.status(400).send({ status: false, msg: "no students from this college has applied yet" })
-        } // since find gives us an array, we can check for its length and in case it's zero, this would be our response
-
-        let details = {
-            name : findcollege.name,
-            fullName : findcollege.fullName,
-            logoLink : findcollege.logoLink,
-            intrests : candidates
-
-        } //creating a details object with all the required keys and values
-
-        return res.status(200).send({ status: true, data: details }) //sending newly created details document
-
-    } catch (error) {
-        
-        return res.status(500).send({ status: false, msg: error.message })
+    //response in case collegeName is not present
+    if (
+      !collegeName ||
+      !/^[a-z]((?![? .,'-]$)[ .]?[a-z]){1,10}$/g.test(collegeName)
+    ) {
+      return res
+        .status(400)
+        .send({
+          status: false,
+          msg: "College Name is required to perform this action",
+        });
     }
-}
+    let findcollege = await collegeModel
+      .findOne({ name: collegeName, isDeleted: false })
+      .select({ _id: 1, name: 1, logoLink: 1, fullName: 1 }); //selecting the required fields
 
-module.exports = {createCollege, collegeDetails}
+    // in caase no college was found, this would be our response
+    if (!findcollege) {
+      return res
+        .status(404)
+        .send({ status: false, msg: "No college with this name exists" });
+    }
+    let collegeId = findcollege._id; //storing the id of the college with find from the name inside a variable collegeId
+
+    //searching for candidates inside the intern model and selecting their name, email, and mobiles
+    let candidates = await internModel
+      .find({ collegeId: collegeId, isDeleted: false })
+      .select({ name: 1, email: 1, mobile: 1 });
+
+    // since find gives us an array, we can check for its length and in case it's zero, this would be our response
+    if (!candidates.length) {
+      return res
+        .status(404)
+        .send({
+          status: false,
+          msg: "no students from this college has applied yet",
+        });
+    }
+    //creating a details object with all the required keys and values
+    let details = {
+      name: findcollege.name,
+      fullName: findcollege.fullName,
+      logoLink: findcollege.logoLink,
+      interns: candidates,
+    }; //sending newly created details document
+    return res.status(200).send({ status: true, data: details });
+  } catch (error) {
+    return res.status(500).send({ status: false, msg: error.message });
+  }
+};
+
+module.exports = { createCollege, collegeDetails };
--- src/models/collegeModel.js
+++ src/models/collegeModel.js
@@ -1,15 +1,13 @@
+const mongoose = require("mongoose");
 
-const mongoose = require('mongoose');
+const collegeSchema = new mongoose.Schema(
+  {
+    name: { type: String, require: true, unique: true, trim: true },
+    fullName: { type: String, require: true, trim: true },
+    logoLink: { type: String, require: true },
+    isDeleted: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
 
-const collegeSchema = new mongoose.Schema({
-name :{type:String, require:true, unique:true, trim:true},
-fullName:{type:String, require:true,  trim:true},
-logoLink:{type:String, require:true},
-isDeleted:{type:Boolean, default:false}
-    
-},{timestamps:true});
-
-module.exports = mongoose.model('college', collegeSchema)
-
-
-
+module.exports = mongoose.model("college", collegeSchema);
